0006f7868a63aa3b7c2536d588d633aba9e9cf86
Johan Jordaan <djjordaan@gmail.com>
2019-06-11 07:12:10 +1000

    new shuffle

--- src/Shuffle.hs
+++ src/Shuffle.hs
@@ -1,7 +1,12 @@
-module Shuffle (shuffle) where
+module Shuffle (shuffle, shuffle') where
 import System.Random
 import Data.Array.IO
 import Control.Monad
+
+import Data.Array.ST
+import Control.Monad.ST
+import Data.STRef
+
 
 -- | Randomly shuffle a list
 --   /O(N)/
@@ -17,4 +22,25 @@ shuffle xs = do
   where
     n = length xs
     newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray n xs =  newListArray (1,n) xs
+
+shuffle' :: [a] -> StdGen -> ([a],StdGen)
+shuffle' xs gen = runST (do
+        g <- newSTRef gen
+        let randomRST lohi = do
+              (a,s') <- liftM (randomR lohi) (readSTRef g)
+              writeSTRef g s'
+              return a
+        ar <- newArray n xs
+        xs' <- forM [1..n] $ \i -> do
+                j <- randomRST (i,n)
+                vi <- readArray ar i
+                vj <- readArray ar j
+                writeArray ar j vi
+                return vj
+        gen' <- readSTRef g
+        return (xs',gen'))
+  where
+    n = length xs
+    newArray :: Int -> [a] -> ST s (STArray s Int a)
     newArray n xs =  newListArray (1,n) xs
